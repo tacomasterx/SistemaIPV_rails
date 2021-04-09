4986a444ab01f396f3eec44a999ec345b7d13818
Tomas Carrillo <tacoman8918@gmail.com>
2021-04-09 16:55:15 -0500

    Added some seeds

--- db/migrate/20201022213935_alter_quantity_from_products.rb
+++
@@ -1,5 +0,0 @@
-class AlterQuantityFromProducts < ActiveRecord::Migration[6.0]
-  def change
-    change_column :products, :quantity, :float
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shop = Shop.create(name: "MATRIZ", pseudonym: "MATRIZ", address: "Matrizlandia 001 col. La Matrix", city:"Zion")
+Employee.create(
+        name: "Invitado",
+        last_name_1: "",
+        last_name_2: "",
+        rfc: "rfc",
+        address: "",
+        phone_home: 0,
+        phone_mobile: 0,
+        charge: "invitado",
+        shop_id: shop.id
+    )
